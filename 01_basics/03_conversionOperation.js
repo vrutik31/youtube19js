7c1dfd86d2e90459c7c3e6ea7b97e6a6e33c8122
vrutik31 <vrutikhirpara01@gmail.com>
2025-03-11 14:01:10 +0000

    conversionOperation

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,40 @@
+//let score = "33abc" // sometimes value is str so first find type
+
+//let score = null
+
+// let score = undefined
+
+// let score = "ined"
+// let score = true
+let score = 1
+
+
+console.log(typeof score)
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber)
+
+console.log(valueInNumber)
+
+console.log("let conversion of boolean")
+
+let abc = ""
+
+
+let z = Boolean(abc)
+console.log(z)
+
+// 1 =>true; 0 => false
+// "" => false
+// "hitesh" => true
+
+
+console.log("let conversion of string")
+
+let vr = 34
+
+let stringnum = String(vr)
+console.log(typeof stringnum)
+
+
+// ********************* Operation *******************
