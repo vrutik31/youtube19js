d343224b33b501d2637740aff6e8468e3b2922e4
vrutik31 <vrutikhirpara01@gmail.com>
2025-03-11 13:41:29 +0000

    data types

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,39 @@
+"use strict"    // treat alljs code as newer version
+
+// alert(3+4) // we are using nodejs not browser
+
+console.log(3+3)
+console.log("vr")
+
+// alert mdn     origonal is https://tc39.es/ecma262/#sec-white-space
+
+let name = "vrutik"
+let age = 89
+let isLoggedIn = false
+
+// number => 2 
+// bigint (big data like stock market)
+// string => ""
+// boolean => true/false
+// null => standalon value. ex: tamprature
+// undefined =>
+// symbol => unique
+
+
+// object
+
+console.log(typeof "vrutik")
+
+console.log(typeof age)
+
+console.log(typeof null) // is object
+console.log(typeof undefined) // is undefined
+
+
+
+let x = age;
+let y = typeof x
+console.log(y)
+
+
+
