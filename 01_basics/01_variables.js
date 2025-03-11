0109a30bd25f4f7e125a7382e5150ae85f766423
vrutik31 <vrutikhirpara01@gmail.com>
2025-03-11 11:09:47 +0000

    config

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,49 @@
+const accountId = 1234567
+let accountEmail = "vrutik@gmail.com"
+var accountPassword = "1234"
+accountCity = "jaipur"
+
+// ddfghdjsbcj
+let vrutik
+
+vgvjgvjvhgv
+// accountId = 2
+
+accountEmail = "v2gm.com"
+accountPassword = "1276757671678"
+accountCity = "surat"
+console.log(accountId);
+
+
+console.table([accountId, accountEmail, accountPassword, accountCity, vrutik])
+
+
+/*
+
+const is no changeable after define
+only have 1 method to declare constant
+
+
+difference btw var and let :
+1)
+var: it ignor block scope so we can access through whole code
+let: not accessible out of function
+
+2)
+var: print without define ---> undefined and go ahead
+let: print without define ---> print syntex error
+
+3)
+var: redeclaration is possible it take value which is in last
+let: redeclaratioin is not possible it say --->  SyntaxError: Identifier 'name' has already been declared
+ 
+accountCity = "jaipur"  is also used for variable but it is not recommanded
+
+
+
+*********** use LET always
+
+console.table([]) is used to print all things in one table form
+*/
+
+
