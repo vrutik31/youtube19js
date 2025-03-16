9d6a08259812451a637ede75f9da407844dc90f4
vrutik31 <vrutikhirpara01@gmail.com>
2025-03-16 14:27:27 +0000

    03_array

---
+++ 02_basics/03_array.js
@@ -0,0 +1,8 @@
+//object declaration : 1)literal 2)constructor
+
+// singleton 
+//constructor thi banavia tyare singleton 1 object bane
+
+
+// object literals
+// not make a singleton its make a multiple instance 
